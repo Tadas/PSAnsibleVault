0bdf7b1c53ed18efd93361490cca7cd2157b9808
Tadas Medišauskas <thetadas+github@gmail.com>
2018-06-02 11:19:40 +0100

    Remove built-in build tools, download dynamically (#2)

--- .build.ps1
+++ .build.ps1
@@ -1,22 +1,40 @@
-﻿Set-StrictMode -Version Latest
+﻿Param(
+	[Version]$OverrideVersion = $null
+)
+Set-StrictMode -Version Latest
 
+# No env vars when running locally...
 if([string]::IsNullOrEmpty($env:APPVEYOR_PROJECT_NAME)) {
 	$ProjectName = ($BuildRoot -split '\\')[-1]
 } else {
 	$ProjectName = $env:APPVEYOR_PROJECT_NAME
 }
-
 $ArtifactPath     = "$BuildRoot\Artifacts"
 $ArtifactFullPath = "$ArtifactPath\$ProjectName.zip"
+$BuildTimeFolder  = "$BuildRoot\BuildTime"
 
 task . <# Analyze, #> Test, SetVersion, BuildArtifact
 
+task Clean {
+	$ArtifactPath,$BuildTimeFolder | ForEach-Object {
+		if (Test-Path -LiteralPath $_) { Remove-Item -LiteralPath $_ -Recurse -Force }
+		New-Item -ItemType Directory -Path $_ -Force | Out-Null
+	}
+}
+
 # Installs dependencies and imports the build utils module itself
-task Install {
+task Install Clean,{
 	Install-Module Pester -Scope CurrentUser -SkipPublisherCheck -Force
 	# Install-Module PSScriptAnalyzer -Scope CurrentUser -Force
 
-	Import-Module "$BuildRoot\BuildTools\MyBuildTools" -Force
+	# Download and import latest version of our build utils module
+	$output_path = Join-Path (Resolve-Path $BuildTimeFolder) "MyBuildTools.zip"
+	$download_url = ((Invoke-RestMethod https://api.github.com/repos/Tadas/MyBuildTools/releases/latest).assets | `
+		Where-Object name -like "MyBuildTools*.zip" | Select-Object -First 1).browser_download_url
+
+	[System.Net.WebClient]::new().DownloadFile($download_url, $output_path)
+	Expand-Archive -LiteralPath $output_path -Destination "$BuildTimeFolder\MyBuildTools"
+	Import-Module "$BuildTimeFolder\MyBuildTools" -Force
 }
 
 # task Analyze Install,{
@@ -54,10 +72,19 @@ task Test Install,{
 
 # Determines the next version and sets it in the appropriate places
 task SetVersion Install,{
-	$LastVersion = Get-LastVersionByTag
-	$LatestCommitMessages = Get-CommitsSinceVersionTag $LastVersion
+	if ($OverrideVersion -eq $null){
+		$LastVersion = Get-LastVersionByTag
+		$LatestCommitMessages = Get-CommitsSinceVersionTag $LastVersion
+		$script:NewVersion = Bump-Version -StartingVersion $LastVersion -CommitMessages $LatestCommitMessages
+	} else {
+		$script:NewVersion = $OverrideVersion
+	}
 
-	$script:NewVersion = Bump-Version -StartingVersion $LastVersion -CommitMessages $LatestCommitMessages
+	# If running in AppVeyor set it's version
+	if(-not [string]::IsNullOrEmpty($env:APPVEYOR_PROJECT_NAME)) {
+		Update-AppveyorBuild -Version "$($script:NewVersion)-$(Get-Date -Format 'yyyymmdd.HHmm')"
+	}
+
 	$script:NewReleaseNotes = ""
 
 	$ManifestFile = "$BuildRoot\$ProjectName.psd1"
@@ -67,15 +94,8 @@ task SetVersion Install,{
 	Out-File -FilePath $ManifestFile -Encoding utf8
 }
 
-task Clean {
-	if (Test-Path -Path $ArtifactPath) {
-		Remove-Item "$ArtifactPath/*" -Recurse -Force
-	}
-	New-Item -ItemType Directory -Path $ArtifactPath -Force | Out-Null
-}
-
 # Builds an artifact into the artifact folder
-task BuildArtifact Install,Clean,{
+task BuildArtifact Clean,SetVersion,{
 	# Should skip this if not on master
 
 	try {
@@ -85,7 +105,7 @@ task BuildArtifact Install,Clean,{
 			(-not $_.FullName.Contains("\.vscode\")) -and
 			(-not $_.FullName.Contains("\.git")) -and
 			(-not $_.FullName.Contains("\Artifacts\")) -and
-			(-not $_.FullName.Contains("\BuildTools\")) -and
+			(-not $_.FullName.Contains("\BuildTime\")) -and
 			(-not $_.FullName.Contains("\Tests\")) -and
 			(-not $_.FullName.EndsWith(".build.ps1")) -and
 			(-not $_.FullName.EndsWith("appveyor.yml"))
@@ -101,19 +121,15 @@ task BuildArtifact Install,Clean,{
 			New-Item -ItemType File -Path $DestinationPath -Force | Out-Null
 			Copy-Item -LiteralPath $_.FullName -Destination $DestinationPath -Force
 		}
-		Compress-Archive -Path "$TempPath\*" -DestinationPath "$ArtifactPath\$ProjectName.zip" -Verbose -Force
+		Compress-Archive -Path "$TempPath\*" -DestinationPath $ArtifactFullPath -Verbose -Force
 
 	} finally {
 		if(Test-Path -PathType Container -LiteralPath $TempPath) { Remove-Item -Recurse $TempPath -Force }
 	}
 }
 
-task DeployGithub <# Analyze, #> Test, SetVersion, BuildArtifact, {
+task DeployGithub <# Analyze, #> Test, BuildArtifact, {
 	# Should skip this if not on master
-
-	$LastVersion = Get-LastVersionByTag
-	$LatestCommitMessages = Get-CommitsSinceVersionTag $LastVersion
-	$NewVersion = Bump-Version -StartingVersion $LastVersion -CommitMessages $LatestCommitMessages
 
 	New-GithubRelease `
 		-Uri          "https://api.github.com/repos/Tadas/$ProjectName/releases" `
